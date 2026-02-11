d14f272e8395ebf6a4db58ce51c5f131b1719124
clinicasonlinepro-ai <clinicasonline.pro@gmail.com>
2026-02-11 19:16:22 -0300

    Excluir script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// Toggle respostas na seção Dúvidas Rápidas
-const questions = document.querySelectorAll(".duvida .question");
-
-questions.forEach(q => {
-  q.addEventListener("click", () => {
-    const answer = q.nextElementSibling;
-    answer.style.display = (answer.style.display === "block") ? "none" : "block";
-  });
-});
